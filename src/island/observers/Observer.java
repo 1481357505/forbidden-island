7b933a8d4b86f39388217f90177450386e5bb998
1481357505 <141431602+1481357505@users.noreply.github.com>
2025-05-30 14:04:23 +0800

    Delete src directory

--- src/island/observers/Observer.java
+++
@@ -1,9 +0,0 @@
-package island.observers;
-
-/**
- * Interface to be implemented by required game play observers
- * @author Eoghan O'Shea and Robert McCarthy
- */
-public interface Observer {
-	public abstract void update(Subject subject);
-}
